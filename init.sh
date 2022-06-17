b42f52b0a89a428d1930aab054a847715513073c
sanchelman <107347191+sanchelman@users.noreply.github.com>
2022-06-18 00:04:13 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo gunicorn -c /home/box/web/hello.py hello:app
